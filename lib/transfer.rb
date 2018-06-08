06399b21c3fd7cfaa6a3c552d622a699477f738d
Natalia Castro <nat.alvz@gmail.com>
2018-06-08 03:29:11 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -13,7 +13,7 @@ class Transfer
   end
   
   def execute_transaction
-    if self.valid? && @status == "pending" 
+    if self.valid? && @status == "pending" && sender.balance > @amount 
       @sender.balance -= @amount
       @receiver.balance += @amount
       @status = "complete"
